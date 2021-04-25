8fc9f536ede8a9c7ee231af53422d3e10198b00b
Brett Story <brettmstory@gmail.com>
2021-04-25 08:08:24 -0500

    Using sharp unicode character instead of hash.

--- src/app/notes.service.ts
+++ src/app/notes.service.ts
@@ -8,19 +8,20 @@ import { Note } from './note';
 export class NotesService {
 
   private readonly flat = '\u{266D}';
+  private readonly sharp = '\u{266F}';
 
   private readonly notes: Note[] = [
     { index: 0, name: 'C', isNatural: true, isGroupedWithC: true},
-    { index: 1, name: 'C# / D' + this.flat, isNatural: false, isGroupedWithC: true},
+    { index: 1, name: 'C' + this.sharp + ' / D' + this.flat, isNatural: false, isGroupedWithC: true},
     { index: 2, name: 'D', isNatural: true, isGroupedWithC: true},
-    { index: 3, name: 'D# / E' + this.flat, isNatural: false, isGroupedWithC: true},
+    { index: 3, name: 'D' + this.sharp + ' / E' + this.flat, isNatural: false, isGroupedWithC: true},
     { index: 4, name: 'E', isNatural: true, isGroupedWithC: true},
     { index: 5, name: 'F', isNatural: true, isGroupedWithC: false},
-    { index: 6, name: 'F# / G' + this.flat, isNatural: false, isGroupedWithC: false},
+    { index: 6, name: 'F' + this.sharp + ' / G' + this.flat, isNatural: false, isGroupedWithC: false},
     { index: 7, name: 'G', isNatural: true, isGroupedWithC: false},
-    { index: 8, name: 'G# / A' + this.flat, isNatural: false, isGroupedWithC: false},
+    { index: 8, name: 'G' + this.sharp + ' / A' + this.flat, isNatural: false, isGroupedWithC: false},
     { index: 9, name: 'A', isNatural: true, isGroupedWithC: false},
-    { index: 10, name: 'A# / B' + this.flat, isNatural: false, isGroupedWithC: false},
+    { index: 10, name: 'A' + this.sharp + ' / B' + this.flat, isNatural: false, isGroupedWithC: false},
     { index: 11, name: 'B', isNatural: true, isGroupedWithC: false}
   ]
 
