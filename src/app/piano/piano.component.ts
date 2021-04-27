b1dc008243221a04f8b7a080695ededf406377f4
Brett Story <brettmstory@gmail.com>
2021-04-27 16:16:22 -0500

    Organizing chords.

--- src/app/piano/piano.component.ts
+++ src/app/piano/piano.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Chord } from '../chord';
 
 @Component({
   selector: 'app-piano',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PianoComponent implements OnInit {
 
+  @Input() chord?: Chord;
+
+  useAlternateLayout: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.chord && this.chord.notes[0]) {
+      this.useAlternateLayout = !this.chord.notes[0].isGroupedWithC;
+    }
   }
 }
