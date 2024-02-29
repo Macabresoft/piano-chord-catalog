80fc110087d54a5e07c57896ee9e5a75be8ed417
Brett Story <brettmstory@gmail.com>
2024-02-28 18:10:39 -0600

    Initial PWA work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,7 @@ import { ChordShapeSelectionComponent } from './chord-shape-selection/chord-shap
 import { KeyComponent } from './chords/piano/key/key.component';
 import { PianoComponent } from './chords/piano/piano.component';
 import { SplitPipe } from './split.pipe';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,13 @@ import { SplitPipe } from './split.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
